4fc8ee501c0d98bb53a04a3f95e19eefb72cb0c4
ArsenicStealth <13sdpuckett@live.com>
2014-09-29 20:32:15 -0600

    A1

--- A1/src/CollectionUtils.java
+++ A1/src/CollectionUtils.java
@@ -1,4 +1,18 @@
+import java.util.Collection;
+
 
 public class CollectionUtils {
-
+	public static void main(String arg[]){
+		
+	}
+	public static int cardinality(Object obj, Collection col){
+		int numobjects = 0;
+		for(int i = 0; i < col.size(); i++){
+			if(col.contains(obj)){
+				numobjects++;
+			}
+		}
+		return numobjects;
+		
+	}
 }
