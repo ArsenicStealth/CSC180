72e578998ad79a9c495bd5f718c395dd7854146d
ArsenicStealth <13sdpuckett@live.com>
2014-09-29 21:43:10 -0600

    A1

--- A1/src/CollectionUtils.java
+++ A1/src/CollectionUtils.java
@@ -1,14 +1,25 @@
+import java.util.List;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 
 public class CollectionUtils {
 	public static void main(String arg[]){
-		
+		Object o = 5;
+		Object i = null;
+		List list = new ArrayList();
+		Object[] ob = new Object[]{5,i,5};
+		list.add(ob);
+		System.out.println(cardinality(o, list));
 	}
 	public static int cardinality(Object obj, Collection col){
 		int numobjects = 0;
-		for(int i = 0; i < col.size(); i++){
-			if(col.contains(obj)){
+		Iterator it = col.iterator();
+		while(it.hasNext()){
+			Object ob = it.next();
+			if(ob == obj){
 				numobjects++;
 			}
 		}
